e4f3bf2e6a00a2db08b3ccf80e93b9c3a6a1be51
DimaMukhin <dima9348@gmail.com>
2017-08-13 17:33:55 -0500

    Fixed un-caught GET request error bug that was breaking further requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,9 +7,27 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city}`;
-  const request = axios.get(url);
+  const request = axios.get(url)
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+      }
+      return console.log(error.config);
+    });
 
-  console.log('Request:', request);
+  console.log('Status', request.status);
 
   return {
     type: FETCH_WEATHER,
